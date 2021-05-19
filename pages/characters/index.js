350a396db1f8f6970012afe0ba965eae75ebca88
VladimirAngelov <wladimirangelow@gmail.com>
2021-05-19 14:03:13 +0300

    bug fix

--- pages/characters/index.js
+++ pages/characters/index.js
@@ -1,11 +1,10 @@
 import {GET_CHARACTERS_QUERY, GRAPHQL_API} from "../../utils/queries";
-import {useEffect, useRef, useState} from "react";
+import {useState} from "react";
 import {request} from 'graphql-request'
 import Navigation from "../../components/nav";
 import CharacterCard from "../../components/characterCard";
 import {CharacterContainer} from './StyledCharacterContainer'
 import {useInView} from "react-intersection-observer";
-import {useRouter} from "next/router";
 
 export const getServerSideProps = async () => {
     const data = await request(GRAPHQL_API, GET_CHARACTERS_QUERY, {
@@ -17,36 +16,20 @@ export const getServerSideProps = async () => {
 const Characters = ({characters, info}) => {
     const [charData, setCharData] = useState(characters)
     const [page, setPage] = useState(info.next)
-    const lastElement = useRef()
-    const {ref, inView, entry} = useInView({rootMargin: '300px'})
-    const router = useRouter();
-    const [isLoading, setIsLoading] = useState(true)
-
-    // useEffect(() => {
-    //     setIsLoading(false)
-    // }, [page]);
+    const {ref, inView} = useInView({rootMargin: '300px'})
 
     const loadMoreData = async (page) => {
         return await request(GRAPHQL_API, GET_CHARACTERS_QUERY, {page})
     }
-
-    const refreshData = () => {
-        router.replace(router.asPath);
-    }
-    // console.log(page)
 
     if (inView) {
         loadMoreData(page)
             .then(data => {
                 if (page !== data.characters.info.next) {
                     setPage(data.characters.info.next)
-                    // setCharData(prevData => ([...prevData, ...data.characters.results]))
                     setCharData([...charData, ...data.characters.results])
-                    // setIsLoading(true)
-                    // console.log(data.characters.info)
                 }
             })
-        console.log(page)
     }
 
     const data = charData?.map(c => <CharacterCard setPage={setPage} key={c.name + c.id} character={c}/>)
